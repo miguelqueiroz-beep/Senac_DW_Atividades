0164ef43c8b040acd0790656659b2a199133e272
analidia07 <miguel.queiroz@estudante.ifto.edu.br>
2026-06-15 21:51:16 -0300

    Aula 5 responsavidade
    como deixar um site responsivel com CSS e inicio da atividade 10

---
+++ ex.009/script.js
@@ -0,0 +1,6 @@
+function menu() {
+    let nav = document.getElementById('nav');
+    let html = document.documentElement;
+    nav.classList.toggle('active')
+    html.classList.toggle('light')
+}
---
+++ ex.010_Atividade/script.js
@@ -0,0 +1,7 @@
+function menu() {
+    let nav = document.getElementById('nav');
+    let html = document.documentElement;
+    nav.classList.toggle('active')
+    html.classList.toggle('light')
+}
+
