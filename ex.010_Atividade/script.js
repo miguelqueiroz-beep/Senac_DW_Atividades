7ed5fe1fd04a42bcd30301757a1f691cd3e4ed42
analidia07 <miguel.queiroz@estudante.ifto.edu.br>
2026-06-17 21:49:31 -0300

    17/06
    Botaõ trocar de cor

--- ex.010_Atividade/script.js
+++ ex.010_Atividade/script.js
@@ -1,5 +1,7 @@
 function menu () {
     let menu = document.getElementById('menu');
+    let html = document.documentElement;
     menu.classList.toggle('active');
+    html.classList.toggle('light')
     
 }
